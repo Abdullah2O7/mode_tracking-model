7afab17a81bd785faf7f54451dfbf9147a0d6121
Abdullah Mohamed <101477947+Abdullah2O7@users.noreply.github.com>
2025-01-18 17:52:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ class Config:
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
     SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
     SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
-    GOOGLE_API_KEY="AIzaSyDOjLjuJzlAEfN7VysS80tdPsGpsNiVZOI"
